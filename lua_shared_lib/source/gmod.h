48e8e5fbb14f25e437e2f0017f2c1a142088bb60
RaphaelIT7 <raphael.i.t@outlook.de>
2024-04-26 20:26:06 +0200

    Fixed a typo

--- lua_shared_lib/source/gmod.h
+++ lua_shared_lib/source/gmod.h
@@ -7,6 +7,6 @@ struct lua_State;
 
 
 DDLL_EXPORT void lua_init_stack_gmod(lua_State* L1, lua_State* L);
-DDLL_EXPORT void GMOD_LuaPrint(const char* str, lua_State* L)
+DDLL_EXPORT void GMOD_LuaPrint(const char* str, lua_State* L);
 DDLL_EXPORT void* GMOD_LuaCreateEmptyUserdata(lua_State* L);
 DDLL_EXPORT const char* GMODLUA_GetUserType(lua_State* L, int iType);
