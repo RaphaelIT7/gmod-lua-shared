5b960967ca41520122f80412f20ce213ca65800e
RaphaelIT7 <raphael.i.t@outlook.de>
2024-04-26 20:50:49 +0200

    Fixed the compile

--- LuaJIT-2.1/src/gmod.h
+++ LuaJIT-2.1/src/gmod.h
@@ -1,4 +1,4 @@
-DDLL_EXPORT void lua_init_stack_gmod(lua_State* L1, lua_State* L);
-DDLL_EXPORT void GMOD_LuaPrint(const char* str, lua_State* L);
-DDLL_EXPORT void* GMOD_LuaCreateEmptyUserdata(lua_State* L);
-DDLL_EXPORT const char* GMODLUA_GetUserType(lua_State* L, int iType);
+extern void lua_init_stack_gmod(lua_State* L1, lua_State* L);
+extern void GMOD_LuaPrint(const char* str, lua_State* L);
+extern void* GMOD_LuaCreateEmptyUserdata(lua_State* L);
+extern const char* GMODLUA_GetUserType(lua_State* L, int iType);
