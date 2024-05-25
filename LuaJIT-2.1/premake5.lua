4ca5b930061f3f8a649d666bdb4748fb2db1641b
RaphaelIT7 <raphael.i.t@outlook.de>
2024-05-25 22:12:21 +0200

    Testing around to build JIT directly

---
+++ LuaJIT-2.1/premake5.lua
@@ -0,0 +1,17 @@
+files({
+	[[src\lib_base.c]],
+	[[src\lib_math.c]],
+	[[src\lib_bit.c]],
+	[[src\lib_string.c]],
+	[[src\lib_table.c]],
+	[[src\lib_io.c]],
+	[[src\lib_os.c]],
+	[[src\lib_package.c]],
+	[[src\lib_debug.c]],
+	[[src\lib_jit.c]],
+	[[src\lib_ffi.c]],
+	[[src\lib_buffer.c]],
+	[[src\lj_opt_fold.c]]
+})
+
+includedirs({[[src\**]]})
--- premake5.lua
+++ premake5.lua
@@ -45,6 +45,9 @@ CreateWorkspace({name = "lua_shared", abi_compatible = false})
 			[[garrysmod_common\sourcesdk-minimal\tier1\convar.cpp]],
 		})
 
+		include([[..\..\LuaJIT-2.1\premake5.lua]])
+		include([[..\..\lua-shared-lib\premake5.lua]])
+
         targetsuffix("")
         libdirs(current_dir .. "/libs")
         filter("system:windows")
